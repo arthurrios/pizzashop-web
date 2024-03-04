74f45ad25d71d84dbf1a01359ceede020a42b056
arthur <arthur.rios007@gmail.com>
2024-03-04 11:39:09 -0300

    test: e2e sign-up

---
+++ test/sign-up.e2e-spec.ts
@@ -0,0 +1,37 @@
+import { expect, test } from '@playwright/test'
+
+test('sign up successful', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Establishment name').fill('Pizza Shop')
+  await page.getByLabel('Your name').fill('John Doe')
+  await page.getByLabel('Your email').fill('johndoe@example.com')
+  await page.getByLabel('Your phone').fill('123124124123')
+
+  await page.getByRole('button', { name: 'Complete sign up' }).click()
+
+  const toast = page.getByText('Restaurant registered succesfully.')
+  expect(toast).toBeVisible()
+})
+
+test('sign up with error', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Establishment name').fill('Invalid name')
+  await page.getByLabel('Your name').fill('John Doe')
+  await page.getByLabel('Your email').fill('johndoe@example.com')
+  await page.getByLabel('Your phone').fill('123124124123')
+
+  await page.getByRole('button', { name: 'Complete sign up' }).click()
+
+  const toast = page.getByText('Error registering restaurant.')
+  expect(toast).toBeVisible()
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Log in' }).click()
+
+  expect(page.url()).toContain('/sign-in')
+})
